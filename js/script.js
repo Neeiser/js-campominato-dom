0f116e42342b03ad1b7e3b8a3ba2d0a3934010bd
Neeiser <stortini.study@gmail.com>
2022-03-03 15:34:38 +0100

    Basic logic comments

--- js/script.js
+++ js/script.js
@@ -64,6 +64,40 @@ playButton.addEventListener('click', function(){
     }
 });
 
+
+/* Logica */
+/*
+- Creo una funzione che genera random 16 numeri interi positivi
+- Li salvo in una variabile arrey
+- 
+*/
+
+function randomBombNumber (min, max) {
+    
+}
+
+
+
+
+
+/* 
+Il computer deve generare 16 numeri casuali nello stesso 
+range della difficoltà prescelta: le bombe.
+I numeri nella lista delle bombe non possono essere duplicati. 
+*/
+
+/* 
+In seguito l'utente clicca su una cella: 
+se il numero è presente nella lista dei numeri generati - 
+abbiamo calpestato una bomba - la cella si colora di rosso e la partita termina, 
+altrimenti la cella cliccata si colora di azzurro e 
+l'utente può continuare a cliccare sulle altre celle.
+La partita termina quando il giocatore clicca su una bomba 
+o raggiunge il numero massimo possibile di numeri consentiti.
+Al termine della partita il software deve comunicare il punteggio, 
+cioè il numero di volte che l’utente ha cliccato su una cella che non era una b. 
+*/
+
 /* 
 reminder 
 - ARREY: .include per i duplicati
